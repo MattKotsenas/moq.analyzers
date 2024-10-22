f10002dc41da1ebf847ab9b7f69b48eeada0417c
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-10-21 20:58:37 -0700

    Add Playground

---
+++ tests/Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Moq;
+using M = Moq.MockBehavior;
+
+Console.WriteLine("Hello, World!");
+
+_ = Mock.Of<ISample>(global::Moq.MockBehavior.Loose);
+
+interface ISample
+{
+    int Calculate() => 0;
+}
